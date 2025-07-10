1d8dbdd673ab2fd51370f276143320bd2d181435
greg.czechowicz <greg@czechowicz.me>
2025-07-10 17:16:21 -0400

    fdsf:q

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA2xay46GlPEvOnxbF6QdkHwarkjbz58B4",
-  authDomain: "czechowiczcruise.firebaseapp.com",
-  projectId: "czechowiczcruise",
-  storageBucket: "czechowiczcruise.firebasestorage.app",
-  messagingSenderId: "1095939303167",
-  appId: "1:1095939303167:web:5719374afe50494c2f4eca",
-  measurementId: "G-HCL38VFVSG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
